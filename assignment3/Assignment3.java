8a28e774b42630dfec5f7068ca4a437d05b50e78
Elsayed Aya Ehab Elsayed Mahmoud <elsaye10@teach.cs.toronto.edu>
2018-03-17 23:49:18 -0400

    TODO add presidential query

--- assignment3/Assignment3.java
+++ assignment3/Assignment3.java
@@ -14,7 +14,7 @@ public class Assignment3 extends JDBCSubmission {
         try{ 
             conn = DriverManager.getConnection(url, username, password);
         } catch(SQLException sqlE) {
-            System.out.println(sqlE);
+            //System.out.println(sqlE);
         }
         return (conn != null);
     }
@@ -24,7 +24,7 @@ public class Assignment3 extends JDBCSubmission {
         try{ 
             conn.close();
         } catch(SQLException sqlE){
-            System.out.println(sqlE);
+            //System.out.println(sqlE);
             return false;
         }
         return true;
@@ -32,22 +32,56 @@ public class Assignment3 extends JDBCSubmission {
 
     @Override
     public ElectionResult presidentSequence(String countryName) {
-            //Write your code here.
-            return null;
+        List<Integer> pids = new ArrayList<Integer>();
+        List<String> parties = new ArrayList<String>();
+        try {
+            String query = "TODO";
+            PreparedStatement execStat = conn.prepareStatement(query); 
+            ResultSet entry = execStat.executeQuery();
+            while (entry.next()) {
+                Integer id = entry.getInt(1); 
+                String party = entry.getString(2);
+                parties.add(party);
+                pids.add(id);
+            }
+        } catch (SQLException sqlE) {
+            //System.out.println(sqlE);
+        }
+        ElectionResult r  = new ElectionResult(pids, parties);
+        return r;
 	}
 
     @Override
     public List<Integer> findSimilarParties(Integer partyId, Float threshold) {
-	//Write your code here.
-        return null;
+        List<Integer> party_ids = new ArrayList<Integer>();
+        try {
+            String query = "SELECT p1.description, p2.id, p2.description FROM party p1 JOIN party p2 WHERE p1.id = %" + partyId + "% AND p1.id <> p2.id AND p1.id";
+            PreparedStatement execStat = conn.prepareStatement(query); 
+            ResultSet entry = execStat.executeQuery();
+            while (entry.next()) {
+                String p1_description = entry.getString(1); 
+                Integer pid2 = entry.getInt(2); 
+                String p2_description = entry.getString(3); 
+                if(similarity(p1_description, p2_description) > threshold) {
+                    party_ids.add(pid2);
+                }
+            }
+        } catch (SQLException sqlE) {
+            //System.out.println(sqlE);
+        }
+        return party_ids;
     }
 
     public static void main(String[] args) throws Exception {
-	    System.out.println("Hellow World");
-        Assignment3 a3 = new Assignment3();
-        String url = "jdbc:postgresql://localhost:5432/csc343h-elsaye10";
-        String username = "elsaye10";
-        a3.connectDB(url, username, "");
+	    //System.out.println("Hellow World");
+        //Assignment3 a3 = new Assignment3();
+        //String url = "jdbc:postgresql://localhost:5432/csc343h-elsaye10";
+        //String username = "elsaye10";
+        //boolean success = a3.connectDB(url, username, "");
+        ////System.out.println("success? " + success);
+        //a3.presidentSequence("France");
+        //success = a3.disconnectDB();
+        ////System.out.println("success? " + success);
     }
 
 }
