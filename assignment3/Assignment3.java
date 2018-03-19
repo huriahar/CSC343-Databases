45dc101c324ac63fd8744c96944acdd628692854
Elsayed Aya Ehab Elsayed Mahmoud <elsaye10@teach.cs.toronto.edu>
2018-03-18 21:44:46 -0400

    added queries

--- assignment3/Assignment3.java
+++ assignment3/Assignment3.java
@@ -3,26 +3,25 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Assignment3 extends JDBCSubmission {
-    private Connection conn = null;
     public Assignment3() throws ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
-        this.conn = null;
+        this.connection = null;
     }
 
     @Override
     public boolean connectDB(String url, String username, String password) {
         try{ 
-            conn = DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(url, username, password);
         } catch(SQLException sqlE) {
             //System.out.println(sqlE);
         }
-        return (conn != null);
+        return (connection != null);
     }
 
     @Override
     public boolean disconnectDB() {
         try{ 
-            conn.close();
+            connection.close();
         } catch(SQLException sqlE){
             //System.out.println(sqlE);
             return false;
@@ -35,8 +34,8 @@ public class Assignment3 extends JDBCSubmission {
         List<Integer> pids = new ArrayList<Integer>();
         List<String> parties = new ArrayList<String>();
         try {
-            String query = "TODO";
-            PreparedStatement execStat = conn.prepareStatement(query); 
+            String query = "SELECT politician_president.id, party.name FROM politician_president JOIN party ON party.id = politician_president.party_id JOIN country ON politician_president.country_id     = country.id WHERE country.name = %" + countryName + "% ORDER BY end_date;";
+            PreparedStatement execStat = connection.prepareStatement(query); 
             ResultSet entry = execStat.executeQuery();
             while (entry.next()) {
                 Integer id = entry.getInt(1); 
@@ -55,14 +54,21 @@ public class Assignment3 extends JDBCSubmission {
     public List<Integer> findSimilarParties(Integer partyId, Float threshold) {
         List<Integer> party_ids = new ArrayList<Integer>();
         try {
-            String query = "SELECT p1.description, p2.id, p2.description FROM party p1 JOIN party p2 WHERE p1.id = %" + partyId + "% AND p1.id <> p2.id AND p1.id";
-            PreparedStatement execStat = conn.prepareStatement(query); 
+            String query = "SELECT p1.description, p2.id, p2.description FROM party p1 JOIN party p2 ON p1.id <> p2.id WHERE p1.id = %" + partyId + "%;";
+            PreparedStatement execStat = connection.prepareStatement(query); 
             ResultSet entry = execStat.executeQuery();
             while (entry.next()) {
                 String p1_description = entry.getString(1); 
                 Integer pid2 = entry.getInt(2); 
                 String p2_description = entry.getString(3); 
-                if(similarity(p1_description, p2_description) > threshold) {
+                System.out.println("p1_des = " + p1_description + " pid2 = " + pid2 + "p2_des = " + p2_description); 
+                Float jaccard_score;
+                if (p1_description == null || p2_description == null) {
+                    jaccard_score = (float)0.0;
+                } else {
+                    jaccard_score = (float)similarity(p1_description, p2_description);
+                }
+                if(jaccard_score >= threshold) {
                     party_ids.add(pid2);
                 }
             }
